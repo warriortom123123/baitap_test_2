6b253a5f04970178c695ac3406a92677156ef9c0
BlackPig147 <nhut014@gmail.com>
2024-03-09 14:00:56 +0700

    4

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
 <form action="login.php" method="post">
     username: <input type="text" name="txtusername"><br>
     Mật khẩu: <input type="password" name="txtmatkhau"><br>
+    <input type="submit" value="Đăng nhập">
+</form>
